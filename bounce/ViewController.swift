51eca3b32bc0b16579f3fcd8b0730170862d9f2f
Alessandra Nishikawa <alessandranishikawa@imacdeaessandra.local>
2017-12-14 23:46:30 -0500

    version_teste

--- bounce/ViewController.swift
+++ bounce/ViewController.swift
@@ -5,21 +5,137 @@
 //  Created by eleves on 17-12-08.
 //  Copyright © 2017 eleves. All rights reserved.
 //
-
+//------------------
 import UIKit
+//------------------
 
 class ViewController: UIViewController {
 
+//------------------
+    @IBOutlet weak var mur_gauche: UIView!
+    @IBOutlet weak var mur_droit: UIView!
+    @IBOutlet weak var mur_haut: UIView!
+    @IBOutlet weak var mur_bas: UIView!
+    @IBOutlet weak var balle: UIView!
+    
+    //--------------------
+    
+    @IBOutlet weak var cible_cent: UIView!
+    @IBOutlet weak var cible_cinquante: UIView!
+    @IBOutlet weak var cible_trente: UIView!
+    @IBOutlet weak var cible_dix: UIView!
+    
+    //--------------------
+    
+    @IBOutlet weak var total_points: UILabel!
+    @IBOutlet weak var points: UILabel!
+    @IBOutlet weak var march_balle: UIView!
+    @IBOutlet weak var jouer: UIButton!
+    
+    //---------------------
+    
+    var objet_bounce: Targetbounce!
+    var cos: Double!
+    var sin: Double!
+    var aTimer: Timer!
+    var acm_points = 0
+
+    
+    //--------------------
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       objet_bounce = Targetbounce(ball: balle,
+                                    left_window: mur_gauche,
+                                    right_window: mur_droit,
+                                    top_window: mur_haut,
+                                    bottom_window: mur_bas,
+                                    target_1: cible_cent,
+                                    target_2: cible_cinquante,
+                                    target_3: cible_trente,
+                                    target_4: cible_dix,
+                                    points: points,
+                                    total_points: total_points,
+                                    move_ball: march_balle,
+                                    start_game: jouer)
+        balle.layer.cornerRadius = 12.5
+        
+        balle.center.x = march_balle.center.x
+ 
+        lancerAnimation()
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //---------
+    
+   
+    
+    
+    
+    //Method pour lancer la animation
+    
+    func lancerAnimation() {
+  
+        acm_points = 0
+        total_points.text = "0"
+        points.text = "0"
+        
+        let degres: Double = Double(arc4random_uniform(360))
+        cos = __cospi(degres/180)
+        sin = __sinpi(degres/180)
+        aTimer = Timer.scheduledTimer(timeInterval: 0.003,
+                                      target: self,
+                                      selector: #selector(animation),
+                                      userInfo: nil,
+                                      repeats: true)
+    
     }
-
+    
+    @objc func animation() {
+        
+        balle.center.x += CGFloat(cos)
+        balle.center.y += CGFloat(sin)
+        
+        if balle.frame.intersects(mur_bas.frame) || balle.frame.intersects(mur_haut.frame) {
+            total_points.text = "0"
+            points.text = "FIN DE JEU!!!"
+            aTimer.invalidate()
+            aTimer = nil
+        }
+        
+        if balle.frame.intersects(cible_cent.frame) {
+            acm_points = acm_points + 100;
+            total_points.text = "\(acm_points)"
+            points.text = "100"
+        }
+        if balle.frame.intersects(cible_cinquante.frame) {
+            acm_points = acm_points + 50;
+            total_points.text = "\(acm_points)"
+            points.text = "50"
+        }
+        if balle.frame.intersects(cible_trente.frame) {
+            acm_points = acm_points + 30;
+            total_points.text = "\(acm_points)"
+            points.text = "30"
+        }
+        if balle.frame.intersects(cible_dix.frame) {
+            acm_points = acm_points + 10;
+            total_points.text = "\(acm_points)"
+            points.text = "10"
+        }
+        
+        sin = objet_bounce.returnCosSinAfterTouch(sin: sin, cos: cos)[0]
+        cos = objet_bounce.returnCosSinAfterTouch(sin: sin, cos: cos)[1]
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesMoved(touches, with: event)
+        let touch: UITouch = touches.first!
+        if touch.view == march_balle{
+            march_balle.center.x = touch.location(in: self.view).x
+        }
+    }
 
 }
 
---
+++ bounce/target_bounce.swift
@@ -0,0 +1,89 @@
+//
+//  target_bounce.swift
+//  
+//
+//  Created by Alessandra Nishikawa on 17-12-13.
+//
+//--------------------------------------------------
+import UIKit
+import Foundation
+//--------------------------------------------------
+//--------------------------------------------------
+class Targetbounce {
+    //--------------------------------------------------
+    var b: UIView!
+    var lw: UIView!
+    var rw: UIView!
+    var tw: UIView!
+    var bw: UIView!
+    var tg1: UIView!
+    var tg2: UIView!
+    var tg3: UIView!
+    var tg4: UIView!
+    var pt: UILabel!
+    var tp: UILabel!
+    var mb: UIView!
+    var sg: UIButton!
+    //--------------------------------------------------
+    init(ball b: UIView,
+         left_window lw: UIView,
+         right_window rw: UIView,
+         top_window tw: UIView,
+         bottom_window bw: UIView,
+         target_1 tg1: UIView,
+         target_2 tg2: UIView,
+         target_3 tg3: UIView,
+         target_4 tg4: UIView,
+         points pt: UILabel,
+         total_points tp: UILabel,
+         move_ball mb: UIView,
+         start_game sg: UIButton) {
+        self.b = b
+        self.lw = lw
+        self.rw = rw
+        self.tw = tw
+        self.bw = bw
+        self.tg1 = tg1
+        self.tg2 = tg2
+        self.tg3 = tg3
+        self.tg4 = tg4
+        self.pt = pt
+        self.tp = tp
+        self.mb = mb
+        self.sg = sg
+    }
+    
+    //--------------------------------------------------
+    func returnCosSinAfterTouch(sin s: Double, cos c: Double) -> [Double] {
+        
+        let r = atan2f(Float(s), Float(c))
+        var d = r * (180 / Float(Double.pi))
+        
+            if b.frame.intersects(lw.frame) || b.frame.intersects(rw.frame) {
+                d = 180 - d }
+            if b.frame.intersects(tw.frame) {
+                let p = abs(d)
+                d=p }
+            if b.frame.intersects(tg1.frame) {
+                let p = abs(d)
+                d=p}
+            if b.frame.intersects(tg2.frame) {
+                let p = abs(d)
+                d=p}
+            if b.frame.intersects(tg3.frame) {
+                let p = abs(d)
+                d=p}
+            if b.frame.intersects(tg4.frame) {
+                let p = abs(d)
+                d=p}
+            if b.frame.intersects(mb.frame) {
+                let n = (d) * -1
+                d=n }
+            if b.frame.intersects(bw.frame){
+              let n = (d) * -1
+                d=n }
+            return [__sinpi(Double(d/180.0)), __cospi(Double(d/180.0))]
+        
+    }
+}
+
